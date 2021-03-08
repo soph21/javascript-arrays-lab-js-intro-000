cd76f2b238ba253bd7c939ac96f7663cbb6eaf4a
Sophie Shin <sophie@alloy.co>
2021-03-08 17:28:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten(name) {
-return kittens.slice(0, kitten.length-1)
+return kittens.slice(0, kittens.length-1)
 }
 function removeFirstKitten(name) {
   return kittens.slice(1)
